ec8ab3aa0504f1132de12d089d2518d427a88e0d
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-09-24 11:06:44 -0300

    create list with for

--- lib/pages/to_do_list_page.dart
+++ lib/pages/to_do_list_page.dart
@@ -1,56 +1,116 @@
-
 import 'package:flutter/material.dart';
 
-class ToDoListPage extends StatelessWidget {
+class ToDoListPage extends StatefulWidget {
   ToDoListPage({super.key});
 
-  // forms to get text from field
-  final TextEditingController emailController = TextEditingController();
+  @override
+  State<ToDoListPage> createState() => _ToDoListPageState();
+}
+
+class _ToDoListPageState extends State<ToDoListPage> {
+  List<String> tasks = [];
+
+  // controllers
+  final TextEditingController tasksController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 232, 232, 232),
+      appBar: AppBar(
+        title: Text(
+          "TO DO LIST",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              TextField(
-                controller: emailController,
-                autocorrect: true,
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                    controller: tasksController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your task',
+                      hintText: 'Example: study flutter',
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        tasks.add(value);
+                        tasksController.clear();
+                      });
+                    },
+                  )),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          tasks.add(tasksController.text);
+                          tasksController.clear();
+                        });
+                      },
+                      style: ButtonStyle(
+                        padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                            EdgeInsets.all(15)),
+                        backgroundColor:
+                            WidgetStatePropertyAll<Color>(Colors.blue),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        size: 30,
+                        color: Colors.white,
+                      )),
+                      
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Flexible(
+                child: ListView(
+                  children: [
+                    for (String task in tasks)
+                      ListTile(
+                        title: Text(task),
+                        subtitle: Text(''),
+                        leading: Icon(Icons.task),
+                        onTap: () => {
+                          print('Task: $task')
+                        },
+                      )
+                  ],
                 ),
-                keyboardType: TextInputType.emailAddress,
-                autofocus: true,
-                onChanged: onChangedEmail,
-                onSubmitted: onSubmittedEmail,
               ),
-              ElevatedButton(
-                onPressed: login, 
-                child: Text("Entrar")
-              ),
+              Row(
+                children: [
+                  Expanded(child: Text("You have a 0 pendents task")),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        tasks.clear();
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll<Color>(Colors.red),
+                    ),
+                    child: Text("Clear"),
+                  )
+                ],
+              )
             ],
           ),
         ),
       ),
     );
-  }
-
-  // get the value from textfield
-  void login(){
-    String text = emailController.text;
-  }
-
-  // get all the modifications in field
-  void onChangedEmail(String text) {
-
-  }
-
-  // when click the sumbit in keyboar
-  void onSubmittedEmail(String text) {
-
   }
 }
